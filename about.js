bb494cea591b4b1acbebf41ed461325d10ee9d98
dallasmay <dmmay9@gmail.com>
2022-06-10 10:26:46 -0600

    completed part 2: edit javascript

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Your form has been submitted!")
+}
+
+const giveCompliment = () => {
+	alert("Daggum, those are some nice sunglasses")
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let duckImg = document.getElementById("duck-pic")
+
+duckImg.addEventListener("mouseover", giveCompliment)
