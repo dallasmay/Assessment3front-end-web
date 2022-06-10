281baf0997da6b9de8582183807f04f2b5f02aac
dallasmay <dmmay9@gmail.com>
2022-06-10 12:39:55 -0600

    added js file, completed parts 4 and 5

---
+++ profile.js
@@ -0,0 +1,15 @@
+const colorBtn = document.getElementById("color");
+const placeBtn = document.getElementById("place");
+const ritualBtn = document.getElementById("ritual");
+
+colorBtn.addEventListener("click", () => {
+    alert("My favorite color is dark blue!")
+});
+
+placeBtn.addEventListener("click", () => {
+    alert("My favorite place is being in a thunderstorm")
+});
+
+ritualBtn.addEventListener("click", () => {
+    alert("My favorite ritual was when I would have to eat Cheez-Its before every high school soccer game I played")
+});
